7e27381fd9480950241f9a2004252ee64faa42e7
abhijitbanerjee05 <itsabhijitbanerjee@gmail.com>
2024-05-20 22:42:15 -0400

    Implements dashboard and sidebar

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({amount}: {amount: number}) => {
+  return (
+    <>
+        <CountUp prefix='$' decimal='.' decimals={2} duration={2.5} end={amount} />
+    </>
+  )
+}
+
+export default AnimatedCounter
