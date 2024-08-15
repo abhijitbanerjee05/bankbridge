6a1d29d120d7ec244bb0074eb6c3cdecc64f9a98
abhijitbanerjee05 <itsabhijitbanerjee@gmail.com>
2024-08-14 22:42:23 -0400

    implements user sign-up flow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,33 @@
+"use client"
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
-import TransactionsTable from '@/components/TransactionsTable'
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import RecentTransactions from '@/components/RecentTransactions';
+import { getGlobalUser } from '@/lib/actions/user.actions';
+import { useRouter } from 'next/navigation';
 
 const Home = () => {
-  const loggedIn = { firstName: 'Abhijit' }
+  const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userData = await getGlobalUser(); // Await the promise
+      if (!userData) {
+        router.push('/sign-up')
+      }
+      setUser(userData); // Set the user data in state
+    };
+
+    fetchUser();
+  }, []);
 
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
           <HeaderBox
-            type="greeting" title="Welcome" user={loggedIn?.firstName || 'Guest'}
+            type="greeting" title="Welcome" user={user?.firstName || 'Guest'}
             subtext="Access and manage your account and transactions!"
           />
 
